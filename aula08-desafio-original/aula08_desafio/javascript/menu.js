2d99fc737c43516daf4cce0d01a13127738c89c4
Lucas Tavares <lucastavaresjd@gmail.com>
2018-03-08 17:14:00 -0300

    desafio

--- aula08-desafio-original/aula08_desafio/javascript/menu.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var $html = document.querySelector('html');
-    var $btn = document.querySelector('.header-nav__hamburgger');
-    
-    $btn.addEventListener('click', function(){
-       $html.classList.toggle('menu-opened') 
-    });
-    
-})();
