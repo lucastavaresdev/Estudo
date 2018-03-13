773d1784f6f549ff1dda42e6f48c7ef47ac6a171
Lucas Tavares <lucastavaresjd@gmail.com>
2018-03-13 09:51:42 -0300

    resultado aula 12

--- aulas/session12/aula12_desafio/beluga.js
+++ aulas/session12/aula12_desafio/beluga.js
@@ -1,21 +1,31 @@
-function calcularMedia(){                    
-                    
+function calcularMedia() {
+
     var total = 0;
     var qtd = arguments.length;
     var x = 0;
     //while(arguments[x]){
-    while(typeof arguments[x] === 'number'){
+    while (typeof arguments[x] === 'number') {
         total += arguments[x];
         x++
-    }                    
+    }
 
     return (total / qtd).toFixed(2);
 }
 
-function sortear(n){
+function sortear(n) {
     var _n = n || 1;
     var nSorteado = Math.random();
     nSorteado = nSorteado * _n;
     nSorteado = Math.floor(nSorteado);
     return nSorteado;
+}
+
+
+function imagem() {
+    var selecionadiv = document.querySelector('.imagem');
+    var numerorandomizado = sortear(5);
+    if (numerorandomizado <= 0 || numerorandomizado >= 6) {
+        sortear(7);
+    } 
+    selecionadiv.innerHTML = '<img src="images/' + numerorandomizado + '.jpg">';
 }
