2d78cb375d2784efa6f9caf423e96acfeeabb9a1
Lucas Tavares <lucastavaresdev@gmail.com>
2018-03-26 14:56:34 -0300

    .

---
+++ aulas/Nova pasta/session12/aula12_desafio/beluga.js
@@ -0,0 +1,27 @@
+function calcularMedia() {
+
+    var total = 0;
+    var qtd = arguments.length;
+    var x = 0;
+    //while(arguments[x]){
+    while (typeof arguments[x] === 'number') {
+        total += arguments[x];
+        x++
+    }
+
+    return (total / qtd).toFixed(2);
+}
+
+function sortear(n) {
+    var _n = n || 1;
+    var nSorteado = Math.random();
+    nSorteado = nSorteado * _n;
+    nSorteado = Math.floor(nSorteado);
+    return nSorteado;
+}
+
+
+function imagem() {
+    var numerorandomizado = sortear(5) + 1;
+    document.body.style.background = "url('images/"+numerorandomizado+".jpg')";
+}
