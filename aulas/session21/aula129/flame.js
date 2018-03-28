8f2ea535d30323323b4b4ce81954d0ea894e5a61
Lucas Tavares <lucastavaresdev@gmail.com>
2018-03-28 11:13:22 -0300

    prototype e array Spread

--- aulas/session21/aula129/flame.js
+++ aulas/session21/aula129/flame.js
@@ -3,6 +3,14 @@ var flame = (function(){
 
     f.$ = document.querySelector .bind(document);
     f.$$ = document.querySelectorAll.bind(document);
+    
+    //todos os arrat tem o get media 
+    Array.prototype.getMedia = function(){
+      //return  _calcularMedia.apply(this,this);
+        //ecma 6 passa cada elemento dentro da array spread operator serve para separar uma array
+        
+        return _calcularMedia(...this);
+    }
 
     function _calcularMedia(){
         let params = arguments;
