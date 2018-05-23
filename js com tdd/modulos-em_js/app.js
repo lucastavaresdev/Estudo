208bfa9d2ce89e1db5002681a52350f09a8bac91
Lucas Tavares <lucastavaresdev@gmail.com>
2018-05-23 17:27:58 -0300

    babel es5

--- js com tdd/modulos-em_js/app.js
+++ js com tdd/modulos-em_js/app.js
@@ -1,8 +1,3 @@
-import *  as R from 'ramda';
+let number = 1
 
-const arr1 = [1,1,1,2,2,3,4,5,6,6];
-const arr2 =[5,6,6,6,7,7,8,9,10,1];
-
-const arr3 = R.union(arr1, arr2);
-
-console.log(arr3);
+console.log(number)
--- js com tdd/modulos-em_js/webpack.config.js
+++ js com tdd/modulos-em_js/webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+	entry: './app',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'assets/js')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			}
+		]
+	}
 };
