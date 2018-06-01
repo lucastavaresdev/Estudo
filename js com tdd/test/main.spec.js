30724adf4147087be0d704a5f3350954281b9559
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-01 13:38:29 -0300

    hooks

--- js com tdd/test/main.spec.js
+++ js com tdd/test/main.spec.js
@@ -1,29 +1,43 @@
-describe('Main', function() {
-    describe('Method A', function() {
-      context('Case 1', function() {
-        it('should happen blabla', function() {
-           // espera que aconteça
-          // Entra de dados / método sum(2,2)
-          // Espera retornar (4) => true | (3) => false => broken test
-         });
-       });
-   
-       context('Case 2', function() {
-         it('should happen blabla', function() {
-          // espera que aconteça
-           // Entra de dados / método sum(2,2)
-           // Espera retornar (4) => true | (3) => false => broken test
-         });
-   
-         it('should happen mimimi', function() {
-           // espera que aconteça
-           // Entra de dados / método sum(2,2)
-           // Espera retornar (4) => true | (3) => false => broken test
-         });
-      });
-     });
-   
-     describe('Method B', function() {
-   
-     });
-   });
+describe('Main', function () {
+
+  //roda uma vez antes do bloco
+  before(function () {
+    console.log(' before');
+  });
+
+
+  //roda uma vez depois do ploco
+  after(() => {
+    console.log('after');
+  });
+
+  //roda todas as vezes, antes de cada bloco
+  beforeEach(() => {
+    console.log('beforeEach');
+  });
+
+  //roda torodas as vezes depois do bloco
+  afterEach(() => {
+    console.log('afterEach');
+  });
+
+
+  it('test1', function () {
+    console.log('teste 1');
+  });
+  it('test2', function () {
+    console.log('teste 2');
+  });
+});
+/*
+before
+beforeEach
+teste 1
+    √ test1
+afterEach
+beforeEach
+teste 2
+    √ test2
+afterEach
+after
+*/
