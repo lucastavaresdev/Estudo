a0b129723c8b0e0d606e3dd72734b0b7de13de3c
lucas tavares <lucas.tavares@ad4pixels.com.br>
2018-06-08 20:09:28 -0300

    Modulando o arquivo de conexao do banco de dados

--- node.js/section05/app/routes/noticias.js
+++ node.js/section05/app/routes/noticias.js
@@ -1,15 +1,11 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function (app) {
 
+    var connection = dbConnection();
+
     app.get('/noticias', function (req, res) {
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'noticias'
-        });
-
+   
         connection.query('select * from noticias', function (erro, resultado) {
             res.render("noticias/noticias" , {noticias : resultado});
         });
