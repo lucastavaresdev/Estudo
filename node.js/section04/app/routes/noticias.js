6f291d5cbd31aa4befec2cb7880d91c10da1555e
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-08 15:49:28 -0300

    dados para criar conexao mysql node

--- node.js/section04/app/routes/noticias.js
+++ node.js/section04/app/routes/noticias.js
@@ -1,5 +1,16 @@
 module.exports = function (app) {
+
+    var mysql = require('mysql');
+
+    var connection = mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        database: 'noticias'
+    });
+
     app.get('/noticias', function (req, res) {
         res.render("noticias/noticias");
     });
 };
+
