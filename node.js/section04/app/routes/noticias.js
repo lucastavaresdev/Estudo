6db9ed0040c14b7c070f88cd665268db99561c18
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-08 17:33:08 -0300

    query

--- node.js/section04/app/routes/noticias.js
+++ node.js/section04/app/routes/noticias.js
@@ -1,16 +1,5 @@
 module.exports = function (app) {
-
-    var mysql = require('mysql');
-
-    var connection = mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : '',
-        database: 'noticias'
-    });
-
     app.get('/noticias', function (req, res) {
         res.render("noticias/noticias");
     });
 };
-
--- node.js/section05/app/routes/noticias.js
+++ node.js/section05/app/routes/noticias.js
@@ -1,5 +1,20 @@
 module.exports = function (app) {
+
     app.get('/noticias', function (req, res) {
-        res.render("noticias/noticias");
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'noticias'
+        });
+
+        connection.query('select * from noticias', function (erro, resultado) {
+            res.send(resultado);
+        });
+
+        //res.render("noticias/noticias");
     });
 };
+
