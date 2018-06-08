b1b6d6d81ee56219ac5d9ecef9fe32a5ef6fe9d8
lucas tavares <lucas.tavares@ad4pixels.com.br>
2018-06-08 20:43:10 -0300

    aplicando o consign no server para importar as rotas

--- node.js/section07/app.js
+++ node.js/section07/app.js
@@ -1,10 +1,10 @@
 var app = require('./config/server.js');
 
-var rotaNoticias = require('./app/routes/noticias')(app);
+// var rotaNoticias = require('./app/routes/noticias')(app);
 
-var rotaHome = require('./app/routes/home')(app);
+// var rotaHome = require('./app/routes/home')(app);
 
-var rotaFormulario = require('./app/routes/formulario_inclusao_noticia')(app);
+// var rotaFormulario = require('./app/routes/formulario_inclusao_noticia')(app);
 
 
 app.listen(5000, function(){
--- node.js/section07/config/server.js
+++ node.js/section07/config/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var consign = require('consign');
+
 var app = express();
 app.set('view engine','ejs');
-app.set('views', './app/views')
+app.set('views', './app/views');
+
+//consign reconhece os modulos e dopois joga na instancia do servidor
+consign().include('app/routes').into(app)
 
 module.exports = app;
