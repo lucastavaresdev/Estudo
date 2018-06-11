e6ad83c247bbff1927ae96fefb0b2d8d2f3b4e1c
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-11 12:35:05 -0300

    conexao

--- node.js/section07/config/server.js
+++ node.js/section07/config/server.js
@@ -6,6 +6,9 @@ app.set('view engine','ejs');
 app.set('views', './app/views');
 
 //consign reconhece os modulos e dopois joga na instancia do servidor
-consign().include('app/routes').into(app)
+consign()
+    .include('app/routes')
+    .then('config/dbConnection.js')//preciso apontar a extenção para nao achar que é subdiretorio
+    .into(app)
 
 module.exports = app;
