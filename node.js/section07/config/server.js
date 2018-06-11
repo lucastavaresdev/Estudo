ef6f1e258cb837d0a0e5039ca0da806b532df00f
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-11 14:53:12 -0300

    consign importando rotas

--- node.js/section07/config/server.js
+++ node.js/section07/config/server.js
@@ -1,6 +1,12 @@
 var express = require('express');
+var consign = require('consign');
+
+
 var app = express();
 app.set('view engine','ejs');
-app.set('views', './app/views')
+app.set('views', './app/views');
+
+//reconhece todos  os arquivos da pasta e depois inclui no servidor
+consign().include('./app/routes').into(app);
 
 module.exports = app;
