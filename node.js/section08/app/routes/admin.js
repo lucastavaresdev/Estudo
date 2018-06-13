eed70ca3340417828f951d9c6deb87f3a0a178d1
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-13 16:47:00 -0300

    insert ok prem nao ta retonando a pagina

--- node.js/section08/app/routes/admin.js
+++ node.js/section08/app/routes/admin.js
@@ -4,8 +4,15 @@ module.exports = function (app) {
     });
 
     app.post('/noticias/salvar',function(req, res){
-        var noticias = req.body;
-        res.send(noticias);
-    });
+        var noticia = req.body;
+
+            var connection = app.config.dbConnection();
+            var noticiasModel = app.app.models.noticiasModel;
     
+            noticiasModel.salvarNoticia(noticia, connection, function (erro, resultado) {
+                res.render("noticias/noticias" , {noticias : resultado});
+            });
+
+    });
+
 };
