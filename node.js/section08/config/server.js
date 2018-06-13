d9945df8af3dd1a47dfa6b05d1bdc56c9255d4e1
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-13 16:15:18 -0300

    recebendo formulario via body-Parser

--- node.js/section08/config/server.js
+++ node.js/section08/config/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.set('view engine','ejs');
 app.set('views', './app/views');
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 //reconhece todos  os arquivos da pasta e depois inclui no servidor
 consign()
