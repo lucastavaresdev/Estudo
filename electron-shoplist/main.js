216ec975290b9e80721905c097f3b2099e09ce4f
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-14 14:04:44 -0300

    electron-criando sub-menu

--- electron-shoplist/main.js
+++ electron-shoplist/main.js
@@ -2,7 +2,7 @@ const electron  = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
@@ -16,4 +16,32 @@ app.on('ready', function(){
         protocol: 'file',
         slashes: true
     }));
+   //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //inset menu
+    Menu.setApplicationMenu(mainMenu)
 });
+
+
+
+//create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File', 
+        submenu:[
+            {
+                label: 'add Item'
+            },
+            {
+                label: 'Clear Item'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin'? 'Command+Q':'Ctrl+Q',
+                click(){
+                        app.quit();
+                }
+            },
+        ]
+    }
+]
