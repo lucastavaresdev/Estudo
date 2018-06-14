09daba7e03c336f1b42edee25a793b4ef0793412
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-14 16:07:37 -0300

    createing screen for add to list

--- electron-shoplist/main.js
+++ electron-shoplist/main.js
@@ -5,11 +5,13 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 //Listen  for app to be ready
 app.on('ready', function(){
     //Create new window
     mainWindow = new BrowserWindow({});
+    
     //Load html into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
@@ -23,6 +25,25 @@ app.on('ready', function(){
 });
 
 
+// handle create add window
+
+function createAddWindow(){
+       //Create new window
+       addWindow = new BrowserWindow({
+           width: 300,
+           height:200,
+           title: 'add Shopping List Item'
+       });
+    
+       //Load html into window
+       addWindow.loadURL(url.format({
+           pathname: path.join(__dirname, 'addWindow.html'),
+           protocol: 'file',
+           slashes: true
+       }));
+
+}
+
 
 //create menu template
 const mainMenuTemplate = [
@@ -30,10 +51,13 @@ const mainMenuTemplate = [
         label: 'File', 
         submenu:[
             {
-                label: 'add Item'
+                label: 'add Item',
+                click(){
+                    createAddWindow();
+                }
             },
             {
-                label: 'Clear Item'
+                label: 'Clear Item',
             },
             {
                 label: 'Quit',
