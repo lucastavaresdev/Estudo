ec6288b59550368bbc47fe47275f486b2bc317d1
lucas tavares <lucas.tavares@ad4pixels.com.br>
2018-06-18 20:41:14 -0300

    section 10 init

---
+++ node.js/section10/config/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+app.set('view engine','ejs');
+app.set('views', './app/views');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//reconhece todos  os arquivos da pasta e depois inclui no servidor
+consign()
+    .include('./app/routes')
+    .then('config/dbConnection.js')//tem que colocar .js
+    .then('app/models')
+    .into(app);
+
+module.exports = app;
