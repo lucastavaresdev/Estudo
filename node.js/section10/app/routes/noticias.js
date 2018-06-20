7b00d5e5dee08d0cb291ef48d6e60045812c3ad7
Lucas Tavares <lucastavaresdev@gmail.com>
2018-06-20 12:36:28 -0300

    retirando a redundancia

--- node.js/section10/app/routes/noticias.js
+++ node.js/section10/app/routes/noticias.js
@@ -12,5 +12,17 @@ module.exports = function (app) {
         });
     });
 
+    //recupera a pagina
+    app.get('/noticia', function (req, res) {
+        //faz a conexao com o banco
+        var connection = app.config.dbConnection();
+        var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+
+        noticiasModel.getNoticia(function (erro, resultado) {
+            res.render("noticias/noticia" , {noticia : resultado});
+        }); 
+    });
+
 };
 
