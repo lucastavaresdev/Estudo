256f035c02be095b12b38326f94208fe8e461b10
Lucas Tavares <lucastavaresdev@gmail.com>
2018-07-25 17:10:45 -0300

    validando formulario

--- node.js/MMORPG baseado na séria de TV Game Of Thrones/app/controllers/cadastro.js
+++ node.js/MMORPG baseado na séria de TV Game Of Thrones/app/controllers/cadastro.js
@@ -3,5 +3,18 @@ module.exports.cadastro = function(application, req, res){
 }
 
 module.exports.cadastrar = function(application, req, res){
-    res.send('vamos cadastrar')
+   
+    var dadosForm = req.body;
+
+    req.assert('nome', 'não pode ser vazio').notEmpty();
+    req.assert('usuario', 'não pode ser vazio').notEmpty();
+    req.assert('senha', 'não pode ser vazio').notEmpty();
+    req.assert('casa', 'não pode ser vazio').notEmpty();
+
+    var erros =  req.validationErrors();
+
+    if(erros){
+        res.send('exitem erros no formulario');
+    }
+    res.send('podemos cadastrar');
 }
