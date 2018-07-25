e1313daed27b1369627ff367f72d91747578f445
Lucas Tavares <lucastavaresdev@gmail.com>
2018-07-25 17:07:12 -0300

    ja redirecionando porem não implementado a logica de cadastrar

--- node.js/MMORPG baseado na séria de TV Game Of Thrones/app/controllers/cadastro.js
+++ node.js/MMORPG baseado na séria de TV Game Of Thrones/app/controllers/cadastro.js
@@ -1,3 +1,7 @@
 module.exports.cadastro = function(application, req, res){
     res.render('cadastro')
 }
+
+module.exports.cadastrar = function(application, req, res){
+    res.send('vamos cadastrar')
+}
