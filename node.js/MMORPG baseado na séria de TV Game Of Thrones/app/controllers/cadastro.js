84172cfe2f4a87fe7d8f21901ad6c10f6bd1a31a
Lucas Tavares <lucastavaresdev@gmail.com>
2018-07-25 17:43:50 -0300

    validando cadastro finalizado

--- node.js/MMORPG baseado na séria de TV Game Of Thrones/app/controllers/cadastro.js
+++ node.js/MMORPG baseado na séria de TV Game Of Thrones/app/controllers/cadastro.js
@@ -1,20 +1,21 @@
 module.exports.cadastro = function(application, req, res){
-    res.render('cadastro')
+    res.render('cadastro', {validacao: {}, dadosForm: {}})
 }
 
 module.exports.cadastrar = function(application, req, res){
    
     var dadosForm = req.body;
 
-    req.assert('nome', 'não pode ser vazio').notEmpty();
-    req.assert('usuario', 'não pode ser vazio').notEmpty();
-    req.assert('senha', 'não pode ser vazio').notEmpty();
-    req.assert('casa', 'não pode ser vazio').notEmpty();
+    req.assert('nome', ' nome não pode ser vazio').notEmpty();
+    req.assert('usuario', ' usuario não pode ser vazio').notEmpty();
+    req.assert('senha', ' senha não pode ser vazio').notEmpty();
+    req.assert('casa', ' casa não pode ser vazio').notEmpty();
 
     var erros =  req.validationErrors();
 
     if(erros){
-        res.send('exitem erros no formulario');
+        res.render('cadastro',{validacao: erros, dadosForm: dadosForm});
+        return;
     }
     res.send('podemos cadastrar');
 }
