a1dab45e66b8433e29874eb2f95dff36f989af29
Lucas Tavares <lucastavaresdev@gmail.com>
2018-07-30 17:39:13 -0300

    autenticação parte 1

--- node.js/MMORPG baseado na séria de TV Game Of Thrones/app/models/UsuariosDAO.js
+++ node.js/MMORPG baseado na séria de TV Game Of Thrones/app/models/UsuariosDAO.js
@@ -23,12 +23,14 @@ UsuariosDAO.prototype.autenticar = function (usuario, req, res) {
 			result.toArray(function (err, result) {
 				if (result[0] != undefined) {
 					req.session.autorizado = true;
+					req.session.usuario = result[0].usuario;
+					req.session.casa = result[0].casa;
 				}
 
 				if (req.session.autorizado) {
-					res.send('usuario encontrado')
+					res.redirect('jogo');
 				} else {
-					res.send('usuario não encontrado')
+					res.render('index', { validacao: {} })
 				}
 
 			})
