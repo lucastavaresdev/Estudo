b2e36ffd0ded732d383e3436ef58129b13c5d243
Lucas Tavares <lucastavaresdev@gmail.com>
2018-08-01 11:39:20 -0300

    configurada rota da view suditos e pergaminhos

--- node.js/MMORPG baseado na séria de TV Game Of Thrones/app/controllers/jogo.js
+++ node.js/MMORPG baseado na séria de TV Game Of Thrones/app/controllers/jogo.js
@@ -14,13 +14,17 @@ module.exports.jogo = function (application, req, res) {
 
     JogoDAO.iniciaJogo(res, usuario, casa);
 
-
-
-
 }
 
 module.exports.sair = function (application, req, res) {
     req.session.destroy(function (err) {
         res.render('index', { validacao: {} });
     });
+}
+
+module.exports.suditos = function (application, req, res) {
+    res.render('aldeoes', { validacao: {} });
+}
+module.exports.pergaminhos = function (application, req, res) {
+    res.render('pergaminhos', { validacao: {} });
 }
