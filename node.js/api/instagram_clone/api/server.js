1c06053e0e0af2eebfad49107d2a3a3ec3dfa066
Lucas Tavares <lucastavaresdev@gmail.com>
2018-08-09 17:11:47 -0300

    fazendo inserção post para o mongo via api

--- node.js/api/instagram_clone/api/server.js
+++ node.js/api/instagram_clone/api/server.js
@@ -1,23 +1,66 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    mongodb = require('mongodb');
+    mongodb = require('mongodb').MongoClient,
+    objectID = require('mongodb').ObjectId;
 
 var app = express();
 
-//bosy-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var port = 8080;
 
-var porta = 8080;
+app.listen(port);
 
-app.listen(porta);
+var dbName = 'instagram';
+var mongoURL = 'mongodb://localhost:27017/' + dbName;
 
+var connMongoDB = function (data) {
+    mongodb.connect(mongoURL, { useNewUrlParser: true }, function (err, client) {
+        var db = client.db(dbName);
+        query(db, data);
+        client.close();
+    });
+}
 
-console.log('Servidor Http esta escultando na porta' + porta);
+function query(db, data) {
+    var collection = db.collection(data.collection);
+    switch (data.operacao) {
+        case 'atualizar':
+            collection.update(data.where, data.set);
+            break;
+        case 'inserir':
+            collection.insertOne(data.dados, data.callback);
+            break;
+        case 'pesquisar':
+            collection.find(data.dados).toArray(data.callback);
+            break;
+        case 'remover':
+            data.where._id = objectID(data.where._id);
+            collection.remove(data.where, data.callback);
+            break;
+    }
+}
+
+console.log('Servidor HTTP escutando na porta ' + port);
 
 app.get('/', function (req, res) {
-    res.send({
-        msg: 'Olá Mundo'
-    })
-})
+    res.send({ msg: 'Olá' });
+});
+
+app.post('/api', function (req, res) {
+    var data = req.body;
+    var dados = {
+        operacao: 'inserir',
+        dados: data,
+        collection: 'postagens',
+        callback: function (err, records) {
+            if (err) {
+                res.json({ 'status': 0 });
+            } else {
+                res.json({ 'status': 1 });
+            }
+        }
+    }
+    connMongoDB(dados);
+});
