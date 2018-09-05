c27097d927dfa3ef5e2759b02eb26506ca6368ea
lucas tavares <lucas.tavares@ad4pixels.com.br>
2018-09-05 20:43:54 -0300

    Componentes Filhos

---
+++ react/exercicios_react/Componentes_Filhos/silvaFamily.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name='Joyce' lastName=' Silva' />
+        <Member name='Lucas' lastName=' Tavares da Silva' />
+    </div>
+)
--- react/exercicios_react/ex/componente.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Primeiro = props => (
-    <h1>Primeiro Componente</h1>
-)
-
-export const Segundo = props => <h2>Segundo Componente</h2>
-
-//export { Primeiro, Segundo }
