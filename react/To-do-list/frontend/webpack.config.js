b60c95f9330287eea63e837516c86fd1121a9c9b
lucas tavares <lucas.tavares@ad4pixels.com.br>
2018-09-12 22:06:39 -0300

    corrigido

--- react/To-do-list/frontend/webpack.config.js
+++ react/To-do-list/frontend/webpack.config.js
@@ -1,20 +1,20 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename: 'app.js'
+        filename: './app.js'
     },
     devServer: {
         port: 8080,
-        contentBase: './public'
+        contentBase: './public',
     },
     resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + '/node_modules/'
+            modules: __dirname + '/node_modules'
         }
     },
     plugins: [
@@ -22,23 +22,19 @@ module.exports = {
     ],
     module: {
         loaders: [{
-                text: /.js[x]?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react'],
-                    plugins: ['transform-object-rest-spread'],
-
-                }
-            },
-            {
-                text: /\.css$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-            },
-            {
-                text: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-                loader: 'file'
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
             }
-        ]
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file'
+        }]
     }
 }
