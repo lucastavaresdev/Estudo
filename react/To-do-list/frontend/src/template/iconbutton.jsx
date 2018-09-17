086e234403645f006235d1e592a5f94cc994d13f
Lucas Tavares <lucastavaresdev@gmail.com>
2018-09-17 17:35:34 -0300

    evento onclick

--- react/To-do-list/frontend/src/template/iconbutton.jsx
+++ react/To-do-list/frontend/src/template/iconbutton.jsx
@@ -3,7 +3,8 @@ import If from './if'
 
 export default props => (
     <If test={!props.hide}>
-        <button className={'btn btn-' + props.style} onClick={props.style}>
+        <button className={'btn btn-' + props.style}
+            onClick={props.onClick}>
             <i className={'fa fa-' + props.icon}></i>
         </button >
     </If>
--- react/To-do-list/frontend/src/todo/todoForm.jsx
+++ react/To-do-list/frontend/src/todo/todoForm.jsx
@@ -4,14 +4,13 @@ import IconButton from '../template/iconbutton'
 
 export default props => (
     <div role='form' className='todoForm'>
-
         <Grid cols='12 9 10'>
             <input type="text" id='description' className='form-control'
                 placeholder='Adicione uma tarefa' ></input>
         </Grid>
-
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
