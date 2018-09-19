ba3247c28626f2072cbd3e92a542079ceafa01d8
Lucas Tavares <lucastavaresdev@gmail.com>
2018-09-19 17:25:52 -0300

    base znode

---
+++ znode/app.js
@@ -0,0 +1,8 @@
+/*importar as configurações do  servidor*/
+
+var app = require('./config/server');
+
+/*Parametrizar a porta de esculta  */
+app.listen(80, function () {
+    console.log('Servidor Online');
+});
