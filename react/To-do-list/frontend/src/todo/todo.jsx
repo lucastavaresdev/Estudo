78f3585e8aa40797b19cf010ec79af0caa862b93
Lucas Tavares <lucastavaresdev@gmail.com>
2018-09-19 12:46:44 -0300

    botoes de marcar e desmarcar

--- react/To-do-list/frontend/src/todo/todo.jsx
+++ react/To-do-list/frontend/src/todo/todo.jsx
@@ -15,6 +15,9 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
 
         this.refresh()
     }
@@ -34,15 +37,36 @@ export default class Todo extends Component {
             .then(resp => this.refresh())
     }
 
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+
+    //pega todos os atributos e so muda o done para true
+    //resposta apos o comando
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => this.refresh())
+    }
 
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm description={this.state.description}
+                <TodoForm
+                    description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} />
-                <TodoList list={this.state.list} />
+                <TodoList
+                    list={this.state.list}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
+                    handleRemove={this.handleRemove} />
             </div>
         )
     }
