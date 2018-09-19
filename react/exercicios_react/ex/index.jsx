740a5528d2c14f1f86f7969651419d48e102bbcc
Lucas Tavares <lucastavaresdev@gmail.com>
2018-09-19 17:05:46 -0300

    redux

--- react/exercicios_react/ex/index.jsx
+++ react/exercicios_react/ex/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 import Field from './field'
 
 
+const reducers = combineReducers({
+    field: () => ({ value: 'opa' })
+})
+
+
 ReactDOM.render(
-    <Field initialValue='Teste' />
-    , document.getElementById('app'))                       
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Teste' />
+    </Provider>
+    , document.getElementById('app'))
+
+
