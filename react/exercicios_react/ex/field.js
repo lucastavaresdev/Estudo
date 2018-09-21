efe18ffc9ebe7a3b6291b83a6cd9032fd489578c
Lucas Tavares <lucastavaresdev@gmail.com>
2018-09-21 11:30:02 -0300

    parte 2 ex 1

--- react/exercicios_react/ex/field.js
+++ react/exercicios_react/ex/field.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react' // importa o react
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeValue } from './fieldActions'
 
 class Field extends Component {//cria a classe e extende para o react
-
     render() {//todo componente deve ter o render
         return (
             <div>
                 <label>{this.props.value}</label><br />
-                <input onChange={this.handleChange} value={this.props.value} />
+                <input onChange={this.props.changeValue} value={this.props.value} />
             </div >
         )
     }
 }
-
 
 function mapStateToProps(state) {
     return {
@@ -20,4 +20,8 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(Field)
+function mapDispatchtoProps(dispatch) {
+    return bindActionCreators({ changeValue }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchtoProps)(Field)
