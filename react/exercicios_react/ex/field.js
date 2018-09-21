f1c559d80f0207978db285338a7beb207f49e041
Lucas Tavares <lucastavaresdev@gmail.com>
2018-09-21 11:17:46 -0300

    parte1 ex 1

--- react/exercicios_react/ex/field.js
+++ react/exercicios_react/ex/field.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react' // importa o react
+import { connect } from 'react-redux'
 
 class Field extends Component {//cria a classe e extende para o react
-    constructor(props) {
-        super(props);
-        this.state = { value: props.initialValue } //atribui o valor inicial para o value
-        this.handleChange = this.handleChange.bind(this);//o this sempre ira apontar para class pois o js altera o referencial do this
-    }
-
-    handleChange(event) {//metodo resposavel para manipular quando houver uma mudança no field
-        this.setState({ value: event.target.value })//quando disparado o evento ele pega a target e atribui o valor
-    }
 
     render() {//todo componente deve ter o render
         return (
             <div>
-                <label>{this.state.value}</label><br />
-                <input onChange={this.handleChange} value={this.state.value} />
+                <label>{this.props.value}</label><br />
+                <input onChange={this.handleChange} value={this.props.value} />
             </div >
         )
     }
 }
 
 
-export default Field
+function mapStateToProps(state) {
+    return {
+        value: state.field.value
+    }
+}
+
+export default connect(mapStateToProps)(Field)
