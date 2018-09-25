68b522d11949e693ddfb385ac02be336c724dcdd
Lucas Tavares <lucastavaresdev@gmail.com>
2018-09-25 13:16:57 -0300

    aula 78

--- react/Todo-app/frontend/src/index.jsx
+++ react/Todo-app/frontend/src/index.jsx
@@ -9,7 +9,11 @@ import reducers from './main/reducers'
 //O arquivo reducer.js(é um reducer) exporta a função com os dados, esses dados são criando 
 //uma store um especie de casulo de dados que é armazenado numa constante depois essa constante é passado 
 // em um provider com a variavel no qual eu posso passar o dado para qualquer componente
-const store = createStore(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+
+const store = createStore(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
