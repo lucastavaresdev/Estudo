57df25df14e2aef1392224e1bbc3ac910d9d284d
Lucas Tavares <lucastavaresdev@gmail.com>
2018-10-02 15:34:43 -0300

    erros no webpack

--- react/ciclos_de_pagamentos/frontend/webpack.config.js
+++ react/ciclos_de_pagamentos/frontend/webpack.config.js
@@ -9,12 +9,12 @@ module.exports = {
     },
     devServer: {
         port: 8080,
-        contentBase: './public'
+        contentBase: './public',
     },
     resolve: {
-        extensions: [' ', '.js', '.jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + '/.node_modules',
+            modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
@@ -29,18 +29,18 @@ module.exports = {
     ],
     module: {
         loaders: [{
-            test: /.js[x]?x/,
+            test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
-                plugin: ['transform-object-rest-spread']
+                plugins: ['transform-object-rest-spread']
             }
         }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            text: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
             loader: 'file'
         }]
     }
