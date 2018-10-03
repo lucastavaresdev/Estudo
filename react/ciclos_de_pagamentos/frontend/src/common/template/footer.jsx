ad522db1f9052de436cdc07dc118a835f5166546
Lucas Tavares <lucastavaresdev@gmail.com>
2018-10-03 11:18:42 -0300

    Componente  Footer

---
+++ react/ciclos_de_pagamentos/frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2018
+            <a href="#" target='' _blank>Lucas Tavares</a>
+
+        </strong>
+    </footer>
+)
