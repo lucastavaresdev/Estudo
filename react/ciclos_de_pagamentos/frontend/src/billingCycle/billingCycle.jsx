3f8ba5e6d1e4d120395a8528d166e903577b3f70
Lucas Tavares <lucastavaresdev@gmail.com>
2018-10-09 15:18:32 -0300

    colocando a aba desejada assim que abrir a aplicação

--- react/ciclos_de_pagamentos/frontend/src/billingCycle/billingCycle.jsx
+++ react/ciclos_de_pagamentos/frontend/src/billingCycle/billingCycle.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
-
 import Tabs from '../common/tab/tabs'
 import TabsHeaders from '../common/tab/tabsHeaders'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
+import { selectTab } from '../common/tab/tabActions'
 
 class BillingCycle extends Component {
+
+    componentWillMount() {
+        this.props.selectTab('tabList')
+    }
+
     render() {
         return (
             <div>
@@ -36,4 +44,5 @@ class BillingCycle extends Component {
     }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)
