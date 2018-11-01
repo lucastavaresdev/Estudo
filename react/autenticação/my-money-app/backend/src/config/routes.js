dd9eb4dd6a1c66440595012906c5fdc93534715f
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-01 16:04:09 -0300

    refazendo rotas

--- react/autenticação/my-money-app/backend/src/config/routes.js
+++ react/autenticação/my-money-app/backend/src/config/routes.js
@@ -1,12 +1,30 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server) {
+module.exports = function (server) {
+    /*
+      *Rotas protegidas por Token Jwt
+      */
 
-    // Definir URL base para todas as rotas 
-    const router = express.Router()
-    server.use('/api', router)
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
-    // Rotas de Ciclo de Pagamento 
+    protectedApi.use(auth)
+
+
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    /*
+    *Rotas abertas
+    */
+
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const authService = require('../api/user/authService')
+    openApi.post('/login', authService.login)
+    openApi.post('/signup', authService.signup)
+    openApi.post('/validateToken', authService.validateToken)
 }
