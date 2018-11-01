4cf3e819b5694782277e5c89bcbd1853cba0e642
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-01 16:31:59 -0300

    corrigindo erros com a apostila

--- react/autenticação/my-money-app/backend/src/api/user/authService.js
+++ react/autenticação/my-money-app/backend/src/api/user/authService.js
@@ -1,27 +1,22 @@
-const _ = require('lodash');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const user = require('./user');
+const _ = require('lodash')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('./user')
 const env = require('../../.env')
-
 const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
-
 const sendErrorsFromDB = (res, dbErrors) => {
     const errors = []
     _.forIn(dbErrors.errors, error => errors.push(error.message))
     return res.status(400).json({ errors })
 }
 
-
-//recebe os dados do usuario e leva para o usuario
 const login = (req, res, next) => {
-    const email = req.body.email || ' '
-    const password = req.body.password || ' '
-
+    const email = req.body.email || ''
+    const password = req.body.password || ''
     User.findOne({ email }, (err, user) => {
         if (err) {
-            return sendErrorsFromDB(res, user)
+            return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
             const token = jwt.sign(user, env.authSecret, {
                 expiresIn: "1 day"
@@ -29,56 +24,48 @@ const login = (req, res, next) => {
             const { name, email } = user
             res.json({ name, email, token })
         } else {
-            return res.status(400).send({ errors: ['Usuario/Senha Invalida'] })
+            return res.status(400).send({ errors: ['Usuário/Senha inválidos'] })
         }
     })
 }
 
-//valida token
-
 const validateToken = (req, res, next) => {
     const token = req.body.token || ''
-
     jwt.verify(token, env.authSecret, function (err, decoded) {
         return res.status(200).send({ valid: !err })
     })
 }
 
-
-//singup
-const singup = (req, res, next) => {
-    const name = req.body.name || ' '
-    const email = req.body.email || ' '
-    const password = req.body.password || ' '
-    const confirmPassword = req.body.confirm_password || ' '
-
+const signup = (req, res, next) => {
+    const name = req.body.name || ''
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    const confirmPassword = req.body.confirm_password || ''
     if (!email.match(emailRegex)) {
-        return res.status(400).send({ errors: ['O e-mail informado está invalido'] })
+        return res.status(400).send({ errors: ['O e-mail informa está inválido'] })
     }
     if (!password.match(passwordRegex)) {
         return res.status(400).send({
             errors: [
-                "Senha Precisa ter: uma letra maiuscula, uma minuscula"
+                "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$ %) e tamanho entre 6 - 20."
             ]
         })
     }
-
     const salt = bcrypt.genSaltSync()
     const passwordHash = bcrypt.hashSync(password, salt)
     if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
-        return res.status(400).send({ errors: ['Senhas não conferem'] })
+        return res.status(400).send({ errors: ['Senhas não conferem.'] })
     }
-
     User.findOne({ email }, (err, user) => {
         if (err) {
             return sendErrorsFromDB(res, err)
         } else if (user) {
-            return res.status(400).send({ errors: ['Usuario já cadastrado'] })
+            return res.status(400).send({ errors: ['Usuário já cadastrado.'] })
         } else {
-            const newUser = new User({ name, emal, password: passwordHash })
+            const newUser = new User({ name, email, password: passwordHash })
             newUser.save(err => {
                 if (err) {
-                    return sendErrorsFromDB(err, err)
+                    return sendErrorsFromDB(res, err)
                 } else {
                     login(req, res, next)
                 }
@@ -86,3 +73,5 @@ const singup = (req, res, next) => {
         }
     })
 }
+
+module.exports = { login, signup, validateToken }
--- react/autenticação/my-money-app/backend/src/config/routes.js
+++ react/autenticação/my-money-app/backend/src/config/routes.js
@@ -1,30 +1,21 @@
 const express = require('express')
 const auth = require('./auth')
-
 module.exports = function (server) {
-    /*
-      *Rotas protegidas por Token Jwt
-      */
-
-    const protectedApi = express.Router()
-    server.use('/api', protectedApi)
-
-    protectedApi.use(auth)
-
-
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(protectedApi, '/billingCycles')
-
-    /*
-    *Rotas abertas
-    */
-
-
-    const openApi = express.Router()
-    server.use('/oapi', openApi)
-
-    const authService = require('../api/user/authService')
-    openApi.post('/login', authService.login)
-    openApi.post('/signup', authService.signup)
-    openApi.post('/validateToken', authService.validateToken)
+  /*
+  * Rotas protegidas por Token JWT
+  */
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
+  protectedApi.use(auth)
+  const BillingCycle = require('../api/billingCycle/billingCycleService')
+  BillingCycle.register(protectedApi, '/billingCycles')
+  /*
+  * Rotas abertas
+  */
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+  const AuthService = require('../api/user/AuthService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 }
