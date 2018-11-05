2f6ff650fa17004f91728410cb22d3e64964c4b8
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-05 16:38:11 -0200

    parte 05 frontend

--- react/autenticação/my-money-app/frontend/src/auth/authReducer.js
+++ react/autenticação/my-money-app/frontend/src/auth/authReducer.js
@@ -1,7 +1,7 @@
 const userKey = '_mymoney_user'
 
 const INITIAL_STATE = {
-    user: { name: 'Teste', email: 'aluno@cod3r.com.br' },
+    user: JSON.parse(localStorage.getItem(userKey)),
     validToken: false
 }
 
