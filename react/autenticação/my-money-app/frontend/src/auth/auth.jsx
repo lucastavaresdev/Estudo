e5a24b350a3d2fa6ad2d754cfe50f2f6420784d1
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-05 16:44:34 -0200

    estrutura do form

--- react/autenticação/my-money-app/frontend/src/auth/auth.jsx
+++ react/autenticação/my-money-app/frontend/src/auth/auth.jsx
@@ -4,6 +4,7 @@ import { reduxForm, Field } from 'redux-form'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { login, signup } from './authActions'
+
 import Row from '../common/layout/row'
 import Grid from '../common/layout/grid'
 import If from '../common/operator/if'
@@ -26,8 +27,38 @@ class Auth extends Component {
         const { loginMode } = this.state
         const { handleSubmit } = this.props
         return (
-            
-         )
+            <div className="login-box">
+                <div className="login-logo"><b> My</b> Money</div>
+                <div className="login-box-body">
+                    <p className="login-box-msg">Bem vindo!</p>
+                    <form onSubmit={handleSubmit(v => this.onSubmit(v))}>
+                        <Field component={Input} type="input" name="name"
+                            placeholder="Nome" icon='user' hide={loginMode} />
+                        <Field component={Input} type="email" name="email"
+                            placeholder="E-mail" icon='envelope' />
+                        <Field component={Input} type="password" name="password"
+                            placeholder="Senha" icon='lock' />
+                        <Field component={Input} type="password" name="confirm_password"
+                            placeholder="Confirmar Senha" icon='lock' hide={loginMode} />
+                        <Row>
+                            <Grid cols="4">
+                                <button type="submit"
+                                    className="btn btn-primary btn-block btn-flat">
+                                    {loginMode ? 'Entrar' : 'Registrar'}
+                                </button>
+                            </Grid>
+                        </Row>
+                    </form>
+                    <br />
+                    <a onClick={() => this.changeMode()}>
+                        {loginMode ? 'Novo usuário? Registrar aqui!' :
+                            'Já é cadastrado? Entrar aqui!'}
+                    </a>
+                </div>
+                <Messages />
+            </div>
+
+        )
     }
 }
 
