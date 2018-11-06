eaae0c877a95fc0d30c627fac8ce4f8699752d45
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-06 10:31:33 -0200

    atualizando as dependencias

--- react/autenticação/my-money-app_atualizado_r16/frontend/src/index.js
+++ react/autenticação/my-money-app_atualizado_r16/frontend/src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
+
+import Routes from './main/routes'
+import reducers from './main/reducers'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
+ReactDOM.render(
+    <Provider store={store}>
+        <Routes />
+    </Provider>
+    , document.getElementById('app'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
