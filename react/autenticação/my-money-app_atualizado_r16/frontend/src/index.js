fc8cf9feb9bb454e678d78ab807fa9f6c9725185
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-06 14:22:09 -0200

    mudanças no front

--- react/autenticação/my-money-app_atualizado_r16/frontend/src/index.js
+++ react/autenticação/my-money-app_atualizado_r16/frontend/src/index.js
@@ -8,7 +8,7 @@ import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
-import Routes from './main/routes'
+import AuthOrApp from './main/authOrApp'
 import reducers from './main/reducers'
 import * as serviceWorker from './serviceWorker';
 
@@ -17,7 +17,7 @@ const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
-        <Routes />
+        <AuthOrApp />
     </Provider>
     , document.getElementById('app'))
 
