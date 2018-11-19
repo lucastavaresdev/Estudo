fe531dacd3083fed4d1298877d23ffd892d811d4
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-19 10:46:56 -0200

    webpack dev e prod

--- react/FundamentosMEAN/webpack4/webpack.config.js
+++ react/FundamentosMEAN/webpack4/webpack.config.js
@@ -1,5 +1,8 @@
+const modoDev = process.env.Node_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin =  require('mini-css-extract-plugin')
+const uglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require ('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -8,12 +11,20 @@ module.exports = {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    optimization: {
+        minimizer: [
+            new uglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     plugins: 
         [
             new MiniCssExtractPlugin({
                 filename: "estilo.css"
             })
-
         ],
     module: {
         rules: [
@@ -25,7 +36,6 @@ module.exports = {
                         // 'style-loader', //adiciona o css a dom injetando a tag style
                         'css-loader', // interpreta @import url()... etc
                         'sass-loader' ,
-                        
                     ]
             }
         ]
