2878dc6d7d6a2001abbbbedba70ebab598eb3e37
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-22 17:41:19 -0200

    componente de cadastro de usuario #02

--- react/crud/frontend/src/components/user/userCrud.jsx
+++ react/crud/frontend/src/components/user/userCrud.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Main from '../templates/main'
+import axios from 'axios'
 
 const headerProps = {
     icon: 'users',
@@ -7,7 +8,37 @@ const headerProps = {
     subtitle: 'Cadastro de usuarios: Incluir, Listar, Alterar e Excluir'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const InitialState = {
+    user: {name: '', email: ''},
+    list: []
+}
+
 export default class UserCrud extends Component {
+   
+    state = { ...InitialState}
+  
+    clear(){
+        this.setState({user: InitialState.user})
+    }
+
+    save(){
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/{user.id}` : baseUrl
+        axios[method](url,user)
+            .then(resp => {
+                    const list = this.getUpdatedList(resp.data)
+                    .this.setState({ user: InitialState.user, list })
+            })
+    }
+
+    getUpdatedList(user){
+        const list = this.state.list.filter(u => u.id !==user.id)
+        list.unshift(user)
+        return list
+    }
+   
     render(){
         return(
             <Main {...headerProps}>
