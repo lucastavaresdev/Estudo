97ce5810372df0e95c391cdff0c2292094cfb2dc
Lucas Tavares <lucastavaresdev@gmail.com>
2018-11-22 14:28:01 -0200

    componente cabeçalho

--- react/crud/frontend/src/components/templates/main.jsx
+++ react/crud/frontend/src/components/templates/main.jsx
@@ -5,7 +5,7 @@ import Header from './header'
 
 export default props => 
   <React.Fragment>
-    <Header>
+    <Header {...props}>
         <main className='content'>
             Conteudo
         </main>
