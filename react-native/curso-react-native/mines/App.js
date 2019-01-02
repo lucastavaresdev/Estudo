c1760fa6e04af2f7ba2c20ac8d568c4a19395fb9
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-02 14:58:14 -0200

    componente campo

--- react-native/curso-react-native/mines/App.js
+++ react-native/curso-react-native/mines/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 import params from './src/params'
 import Field from './src/components/Field'
 
@@ -11,6 +11,12 @@ export default class App extends Component{
       <Text style={styles.instructions}>Tamanho da Grade:
       {params.getRowsAmount()}x{params.getColumnsAmount()} </Text>
       <Field /> 
+      <Field opened /> 
+      <Field opened nearMines={1} /> 
+      <Field opened nearMines={2} /> 
+      <Field opened nearMines={3} /> 
+      <Field opened nearMines={4} /> 
+      <Field opened nearMines={6} /> 
 
       </View>
     );
