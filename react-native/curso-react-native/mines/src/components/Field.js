e866e696d15a329d2ab837b07f6775c560671d8d
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-02 16:18:21 -0200

    flag

--- react-native/curso-react-native/mines/src/components/Field.js
+++ react-native/curso-react-native/mines/src/components/Field.js
@@ -2,15 +2,17 @@ import React from 'react'
 import {View, StyleSheet, Text} from 'react-native'
 import params from '../params'
 import Mine from './Mine'
+import Flag from './Flag'
 
 export default props => {
-    const {mined, opened, nearMines, exploded} = props
+    const {mined, opened, nearMines, exploded, flagged} = props
     const styleField = [styles.field]
     
     
     if (opened) styleField.push(styles.field)
     if (exploded) styleField.push(styles.exploded)
-    if(styleField.length ===1 ) styleField.push(styles.regular)
+    if (flagged) styleField.push(styles.flagged)
+    if(!opened  && !exploded) styleField.push(styles.regular)
 
     let color = null
     if(nearMines > 0){
@@ -28,6 +30,7 @@ export default props => {
                         {nearMines}
                 </Text>: false  }
                 {mined && opened ? <Mine /> : false}
+                {flagged && !opened ? <Flag /> : false}
         </View>
     )
 }
@@ -39,14 +42,14 @@ const styles = StyleSheet.create({
         borderWidth: params.borderSize,
     },
     regular: {
-        backgroundColor: '#999',
+        backgroundColor: '#696969',
         borderLeftColor: '#CCC',
         borderTopColor: '#CCC',
         borderRightColor: '#333',
         borderBottomColor: '#333',
     },
     opened: {
-        backgroundColor: '#999',
+        backgroundColor: '#696969',
         borderColor: '#777',
         alignItems: 'center',
         justifyContent: 'center',
