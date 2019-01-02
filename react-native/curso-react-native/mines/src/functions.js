f6905dbf116e8b914043c4824189b557846cbc38
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-02 17:43:50 -0200

    componente Tabuleiro

--- react-native/curso-react-native/mines/src/functions.js
+++ react-native/curso-react-native/mines/src/functions.js
@@ -1,4 +1,3 @@
-
 const createBoard = (rows, columns) => {
     return Array(rows).fill(0).map((_, row) => {
         return Array(columns).fill(0).map((_, column) => {
@@ -20,21 +19,21 @@ const spreadMines = (board, minesAmount) => {
     const columns = board[0].length
     let minesPlanted = 0
     
-    while (minesPlanted < minesAmount){
+    while (minesPlanted < minesAmount) {
         const rowSel = parseInt(Math.random() * rows, 10)
-        const columnsSel = parseInt(Math.random() * columns, 10)
+        const columnSel = parseInt(Math.random() * columns, 10)
 
-        if(!board[rowSel][columnsSel].mined){
-            board[rowSel][columnsSel].mined = true
+        if (!board[rowSel][columnSel].mined) {
+            board[rowSel][columnSel].mined = true
             minesPlanted++
         }
     }
 }
 
-const createMineBoard = (rows, columns, minesAmount) => {
+const createMinedBoard = (rows, columns, minesAmount) => {
     const board = createBoard(rows, columns)
     spreadMines(board, minesAmount)
     return board
 }
 
-export {createMineBoard}
+export {createMinedBoard}
