5de03c9bb17e15ffb1a8a772bf18ccd1923b3072
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-02 16:30:45 -0200

    flag maior

--- react-native/curso-react-native/mines/App.js
+++ react-native/curso-react-native/mines/App.js
@@ -10,18 +10,18 @@ export default class App extends Component{
       <Text style={styles.welcome}>Iniciando o Mines !!</Text>
       <Text style={styles.instructions}>Tamanho da Grade:
       {params.getRowsAmount()}x{params.getColumnsAmount()} </Text>
-      <Field /> 
-      <Field opened /> 
-      <Field opened nearMines={1} /> 
-      <Field opened nearMines={2} /> 
-      <Field opened nearMines={3} /> 
-      <Field opened nearMines={4} /> 
-      <Field opened nearMines={6} /> 
-      <Field mined /> 
-      <Field mined opened /> 
-      <Field mined opened exploded /> 
-      <Field flagged /> 
-      <Field flagged opened/> 
+        <Field /> 
+        <Field opened /> 
+        <Field opened nearMines={1} /> 
+        <Field opened nearMines={2} /> 
+        <Field opened nearMines={3} /> 
+        <Field opened nearMines={4} /> 
+        <Field opened nearMines={6} /> 
+        <Field mined /> 
+        <Field mined opened /> 
+        <Field mined opened exploded /> 
+        <Field flagged /> 
+        <Field flagged opened/> 
       </View>
     );
   }
