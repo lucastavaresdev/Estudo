26fa5b317e5b06ec514a6a0086ed7005de4146ac
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-10 17:37:10 -0200

    react app com problema

--- react-native/curso-react-native/tasks/src/screens/Agenda.js
+++ react-native/curso-react-native/tasks/src/screens/Agenda.js
@@ -1,4 +1,4 @@
-import React,{Componenet} from 'react'
+import React,{Component} from 'react'
 import {StyleSheet, Text, View, ImageBackgroud} from 'react-native'
 import moment from 'moment'
 import 'moment/locale/pt-br'
@@ -8,7 +8,7 @@ import commonStyles from '../commonStyles'
 export default class Agenda extends Component {
     render(){
         return(
-            <View style={StyleSheet.container}>
+            <View style={styles.container}>
                 <ImageBackgroud source={todayImage} style={styles.backgroud}>
                     <View style={styles.titlebar}>
                         <Text style={styles.title}>Hoje</Text>
@@ -17,7 +17,43 @@ export default class Agenda extends Component {
                         </Text>
                     </View>
                 </ImageBackgroud>
+                <View style={styles.taskContainer}>
+                    <Text>Tarefa 1</Text>
+                    <Text>Tarefa 2</Text>
+                    <Text>Tarefa 3</Text>
+                    <Text>Tarefa 3</Text>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    backgroud: {
+        flex: 3,
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title: {
+            fontFamily: commonStyles.fontFamily,
+            color: commonStyles.colors.secondary,
+            fontSize: 50,
+            marginLeft: 20,
+            marginBottom: 10,
+        },
+        subtitle: {
+            fontFamily: commonStyles.fontFamily,
+            color: commonStyles.colors.secondary,
+            fontSize: 20,
+            marginLeft: 20,
+            marginBottom: 30,
+        },
+        taksContainer: {
+        flex: 7,
+     }
+})
