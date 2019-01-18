e213aa9cd31a4ff01ac93c35740e1a9ed0eb1c86
Lucas Tavares <lucastavaresdev@gmail.com>
2019-01-18 17:03:18 -0200

    middeleware

--- react-native/curso-react-native/tasks-backend/index.js
+++ react-native/curso-react-native/tasks-backend/index.js
@@ -1,6 +1,12 @@
 const app = require('express')()
+const db = require('./config/db')
+const consign = require('consign')
 
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
 
+app.db = db
 
 app.listen(3000, () => {
     console.log('Backend execultando...')
