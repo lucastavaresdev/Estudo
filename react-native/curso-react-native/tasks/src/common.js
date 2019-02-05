f1482475d3e85ca9bcef6d42ec1ebe270b82a73a
Lucas Tavares <lucastavaresdev@gmail.com>
2019-02-05 16:56:22 -0200

    localhost da api

--- react-native/curso-react-native/tasks/src/common.js
+++ react-native/curso-react-native/tasks/src/common.js
@@ -1,7 +1,7 @@
 import { Alert, Platform } from 'react-native'
 
 const server = Platform.OS === 'ios' ?
-    'http://localhost:3000' : 'http://10.0.2.2:3000'
+    'http://localhost:3005' : 'http://10.0.2.2:3005'
 
 function showError(err) {
     Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err}`)
