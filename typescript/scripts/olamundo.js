eda9876556daa7ed2d32544adfa950fc1425039f
Lucas <lucastavaresdev@gmail.com>
2019-03-28 10:56:53 -0300

    aula 3

---
+++ typescript/scripts/olamundo.js
@@ -0,0 +1,6 @@
+"use strict";
+var p = document.createElement('p');
+var ola = "Joyce Tomiati";
+p.textContent = ola;
+document.body.appendChild(p);
+//# sourceMappingURL=olamundo.js.map
---
+++ typescript/scripts/olamundo.ts
@@ -0,0 +1,4 @@
+var p = document.createElement('p');
+var ola: string = "Joyce Tomiati";
+p.textContent = ola;
+document.body.appendChild(p);
