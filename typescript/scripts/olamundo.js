aad5aac56a3e549233f72bf3c7082271494748fc
Lucas <lucastavaresdev@gmail.com>
2019-03-28 11:21:29 -0300

    aula 4 - tipos primitivos

--- typescript/scripts/olamundo.js
+++ typescript/scripts/olamundo.js
@@ -3,4 +3,33 @@ var p = document.createElement('p');
 var ola = "Joyce Tomiati";
 p.textContent = ola;
 document.body.appendChild(p);
+var num = 10;
+var bool = true;
+var str = 'Uma mensagem';
+var templatestr = str + " de lucas";
+console.log(templatestr);
+function func() { }
+if (bool != null) { }
+if (bool != undefined) { }
+if (bool) {
+    var i = void 0;
+    for (i = 0; i < 3; i++) {
+        console.log(i);
+    }
+}
+var list = [1, 2, 3];
+var list2 = [1, 2, 3];
+var tuple;
+tuple = ['lucas', 22];
+console.log(tuple[0].toLowerCase());
+console.log(tuple[1]);
+var Day;
+(function (Day) {
+    Day[Day["Monday"] = 0] = "Monday";
+    Day[Day["Tuesday"] = 2] = "Tuesday";
+    Day[Day["Wednesday"] = 3] = "Wednesday";
+})(Day || (Day = {}));
+var day = Day.Monday;
+console.log("day: " + day + " || " + Day.Tuesday + " || " + Day.Wednesday);
+console.log("day: " + Day[0] + " ");
 //# sourceMappingURL=olamundo.js.map
--- typescript/scripts/olamundo.ts
+++ typescript/scripts/olamundo.ts
@@ -2,3 +2,35 @@ var p = document.createElement('p');
 var ola: string = "Joyce Tomiati";
 p.textContent = ola;
 document.body.appendChild(p);
+
+let num : number = 10;
+let bool: boolean = true;
+let str: string = 'Uma mensagem'
+let templatestr: string = `${str} de lucas`
+
+console.log(templatestr)
+
+function func():void{}
+if(bool != null){}
+if(bool != undefined){}
+
+if(bool){
+    let i: number;
+    for(i = 0 ; i<3;i++){
+        console.log(i)
+    }
+}
+
+let list: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3]
+
+let tuple:[string, number]
+tuple=['lucas',22]
+
+console.log(tuple[0].toLowerCase())
+console.log(tuple[1])
+
+enum Day {Monday, Tuesday  = 2, Wednesday}
+let day: Day = Day.Monday
+console.log(`day: ${day} || ${Day.Tuesday} || ${Day.Wednesday}` )
+console.log(`day: ${Day[0]} `)
