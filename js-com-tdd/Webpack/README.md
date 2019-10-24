5a7c7b0cb376fa425adda9a5b112d5e3fad46af7
Lucas Tavares <lucastavares@ioasys.com>
2019-10-24 17:31:20 -0300

    webpack com babel 7 falta configuração

---
+++ js-com-tdd/Webpack/README.md
@@ -0,0 +1,9 @@
+# webpack
+
+Aula sobre o webpack
+
+## Start
+
+1) npm i -y
+
+2) npm install --save-dev webpack
---
+++ js-com-tdd/Webpack/webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: '.app.js',
+    output: {
+        filename: './build.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        }]
+    }
+}
