9850c79d8d79ccd099f7cadb50aeb01a9577af55
Lucas Tavares <lucastavares@ioasys.com>
2020-04-23 14:49:21 -0300

    Animações no android

---
+++ kotlin/DefautTeste/app/src/main/java/com/lucastomiati/defautteste/Animation.kt
@@ -0,0 +1,12 @@
+package com.lucastomiati.defautteste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Animation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animation)
+    }
+}
